dbf59835659aa4eb3f84a07f7559150ade0684c0
gregoriocojr <gregoriocojr@gmail.com>
2025-11-02 18:11:23 +0800

    week 1 commit for array

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 public static class Arrays
 {
     /// <summary>
@@ -13,7 +15,22 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // PLAN for Problem 1
+        // 1. Create an empty List<double> to store the multiples.
+        // 2. Use a loop variable i from 1 to 'length'.
+        // 3. Compute multiple = number * i.
+        // 4. Add the computed mulitple to the list of nultiples.
+        // 5. Print the list to an array.
+
+        var multiples = new List<double>();
+
+        for (int i = 1; i <= length; i++)
+        {
+            double multiple = number * i;
+            multiples.Add(multiple);
+        }
+
+        return multiples.ToArray(); // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +46,27 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // PLAN for Problem 2 (assumption: amount <= data.Count)
+        // 1. Initialize the n = data.Count (number of items in 'data').
+        // 2. Create a tempList1<int> for the rightmost 'data' as determined by the 'amount'.
+        // 3. Create a tempList2<int> for the remaining 'data' from the original list.
+        // 4. Clear the original list.
+        // 5. Add the range from tempList1, then tempList2.
+        // 6. Return the array.
+
+        int n = data.Count;
+
+        List<int> tempList1 = data.GetRange(n - amount, amount);
+
+        List<int> tempList2 = data.GetRange(0, n - amount);
+
+        data.Clear();
+
+        data.AddRange(tempList1);
+
+        data.AddRange(tempList2);
+
+        return;
     }
 }
